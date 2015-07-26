cf0cf58679262e32076414b760a8c65df48fa21c
Łukasz Grolik <lukasz@grolik.pl>
2015-07-26 19:23:39 +0200

    Update

--- src/app/lib/repo/Repo.js
+++ src/app/lib/repo/Repo.js
@@ -1,23 +1,23 @@
 angular.module('app')
 .factory('Repo', [function() {
 
-	const DATE_FORMAT = 'YYYY-MM-DD HH:mm';
+  const DATE_FORMAT = 'YYYY-MM-DD HH:mm';
 
-	function Repo(body) {
-		this.name = body.name;
-		this.description = body.description;
-		this.homepage = body.homepage;
-		this.createdAt = moment(body.createdAt).format(DATE_FORMAT);
-		this.updatedAt = moment(body.updatedAt).format(DATE_FORMAT);
-		this.pushedAt = moment(body.pushedAt).format(DATE_FORMAT);
-		this.commitsCount = body.commitsCount;
-		this.contributorsCount = body.contributorsCount;
-		this.watchersCount = body.watchersCount;
-		this.starsCount = body.starsCount;
-		this.forksCount = body.forksCount;
-		this.openIssuesCount = body.openIssuesCount;
-	}
+  function Repo(body) {
+    this.name = body.name;
+    this.description = body.description;
+    this.homepage = body.homepage;
+    this.createdAt = moment(body.createdAt).format(DATE_FORMAT);
+    this.updatedAt = moment(body.updatedAt).format(DATE_FORMAT);
+    this.pushedAt = moment(body.pushedAt).format(DATE_FORMAT);
+    this.commitsCount = body.commitsCount;
+    this.contributorsCount = body.contributorsCount;
+    this.watchersCount = body.watchersCount;
+    this.starsCount = body.starsCount;
+    this.forksCount = body.forksCount;
+    this.openIssuesCount = body.openIssuesCount;
+  }
 
-	return Repo;
+  return Repo;
 
 }]);
--- src/app/lib/user-recent-requests/UserRecentRequests.js
+++ src/app/lib/user-recent-requests/UserRecentRequests.js
@@ -1,27 +1,25 @@
 angular.module('app')
 .service('UserRecentRequests', function() {
 
-	this.REPOS_REQUESTS_STORAGE_KEY = 'reposRequests'
-	this.REPOS_LIMIT = 5;
-	this.requests = JSON.parse(localStorage.getItem(this.REPOS_REQUESTS_STORAGE_KEY)) || [];
-	this.globalRequests = [];
+  this.REPOS_REQUESTS_STORAGE_KEY = 'reposRequests'
+  this.REPOS_LIMIT = 5;
+  this.requests = JSON.parse(localStorage.getItem(this.REPOS_REQUESTS_STORAGE_KEY)) || [];
+  this.globalRequests = [];
 
-	this.add = function(reposNames) {
-		var _this = this;
+  this.add = function(reposNames) {
+    this.requests.forEach(function(request, i) {
+      if (request.join() === reposNames.join()) {
+        this.requests.splice(i, 1);
+      }
+    }.bind(this));
 
-		this.requests.forEach(function(request, i) {
-			if (request.join() === reposNames.join()) {
-				_this.requests.splice(i, 1);
-			}
-		});
+    this.requests.unshift(reposNames);
 
-		this.requests.unshift(reposNames);
+    if (this.requests.length > this.REPOS_LIMIT) {
+      this.requests.length = this.REPOS_LIMIT;
+    }
 
-		if (this.requests.length > this.REPOS_LIMIT) {
-			this.requests.length = this.REPOS_LIMIT;
-		}
-
-		localStorage.setItem(this.REPOS_REQUESTS_STORAGE_KEY, JSON.stringify(this.requests));
-	}
+    localStorage.setItem(this.REPOS_REQUESTS_STORAGE_KEY, JSON.stringify(this.requests));
+  }
 
 });
